f4ab0ae93062ac6e3cf5d59b07f3cb3f9a778ff5
lnoliver96 <luanotoni4@gmail.com>
2024-04-02 11:04:36 -0300

    Commit Inicial

---
+++ app/src/main/java/oliveira/otoni/emailapp/MainActivity.java
@@ -0,0 +1,56 @@
+package oliveira.otoni.emailapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        //Definição da ação do click do botao
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Obtendo dados digitados pelo usuario
+                EditText etEmail = findViewById(R.id.etEmail);
+                String email = etEmail.getText().toString();
+
+                EditText etAssunto = findViewById(R.id.etAssunto);
+                String assunto = etEmail.getText().toString();
+
+                EditText etTexto = findViewById(R.id.etTexto);
+                String texto = etEmail.getText().toString();
+
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+
+                i.setData(Uri.parse("mailto: "));
+
+                String[] emails = new String[]{email};
+                i.putExtra(Intent.EXTRA_EMAIL,emails);
+                i.putExtra(Intent.EXTRA_SUBJECT, assunto);
+                i.putExtra(Intent.EXTRA_TEXT, texto);
+
+                try {
+                    startActivity(Intent.createChooser(i,"Escolha o APP"));
+
+                }
+                catch (ActivityNotFoundException e){
+                    Toast.makeText(MainActivity.this,"Não há nenhum app que posso realizar essa operação", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+}
